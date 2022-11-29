7b49f6380bedb984eb416fbd99c6b9b198aca4c3
Zakharov E.V <jenia_2510@mail.ru>
2022-11-29 13:05:22 +0500

    Install Stream Chat SDK

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import "react-native-gesture-handler";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -8,6 +8,8 @@ import Navigation from './navigation';
 import { NhostClient, NhostReactProvider } from '@nhost/react';
 import * as SecureStore from 'expo-secure-store';
 import { NhostApolloProvider } from "@nhost/react-apollo";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 const nhost = new NhostClient({
   subdomain: 'lbintduddvtmicbigcct',
   region: 'eu-central-1',
@@ -23,14 +25,16 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <NhostReactProvider nhost={nhost}>
-          <NhostApolloProvider nhost={nhost}>
-            <Navigation colorScheme={colorScheme} />
-          </NhostApolloProvider>
-        </NhostReactProvider>
-        <StatusBar />
-      </SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaProvider>
+          <NhostReactProvider nhost={nhost}>
+            <NhostApolloProvider nhost={nhost}>
+              <Navigation colorScheme={colorScheme} />
+            </NhostApolloProvider>
+          </NhostReactProvider>
+          <StatusBar />
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
     );
   }
 }
