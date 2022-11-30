fff251a304bc1fa33545022317102a7b4d4a2abc
Zakharov E.V <jenia_2510@mail.ru>
2022-11-30 14:09:25 +0500

    Connect to stream chat sdk and create a user

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -1,12 +1,48 @@
-import React, { createContext, useContext } from "react";
+import { useUserData } from "@nhost/react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
 
 export const ChatContext = createContext({});
 
 const ChatContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const [chatClient, setChatClient] = useState<StreamChat>();
+
+    const user = useUserData();
+
+    useEffect(() => {
+        const initChat = async () => {
+            if (!user) {
+                return;
+            }
+
+            const client = StreamChat.getInstance("mwc7jp6w96df");
+
+            await client.connectUser(
+                {
+                    id: user.id,
+                    name: user.displayName,
+                    image: user.avatarUrl,
+                },
+                client.devToken(user.id)
+            );
+
+            setChatClient(client);
+        };
+
+        initChat();
+    }, []);
+
+    useEffect(() => {
+        return () => {
+            if (chatClient) {
+                chatClient.disconnectUser();
+            }
+        };
+    }, []);
+
     const value = { username: "test context" };
     return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
-
 export const useChatContext = () => useContext(ChatContext);
-
 export default ChatContextProvider;
